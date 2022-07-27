c7b48c306d21d90f9f237bb4178d3663d2cea263
E-Lop <edmondolopez@gmail.com>
2022-07-27 15:06:29 +0200

    esercizio completato

--- index.php
+++ index.php
@@ -44,8 +44,7 @@ $tron->year = 1982;
 $tron->director = 'Steven Lisberger';
 $tron->poster = 'https://image.tmdb.org/t/p/w342/yObE2IlhxqVA17dbLtd38Ojhmhz.jpg';
 
-var_dump($akira->howLongAgo());
-var_dump($akira);
+
 ?>
 
 <!DOCTYPE html>
@@ -58,6 +57,52 @@ var_dump($akira);
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <main>
+        <h1><span class="strikeout">Sentiti vecchio</span> Fai un tuffo nel passato con questi classici del cinema</h1>
+        <div class="box">
+            <div class="locandina">
+                <img src="<?php echo $akira->poster; ?>" alt="<?php echo 'Locandina di' . ' ' . $akira->title ?>">
+            </div>
+            <div class="movie_data">
+                <h2>Titolo: <?php echo $akira->title; ?></h2>
+                <p>Genere: <?php echo $akira->genre; ?></p>
+                <p>Regista: <?php echo $akira->director; ?></p>
+                <p class="horrible_truth">Uscito al cinema <span><?php echo $akira->howLongAgo(); ?></span> anni fa</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="locandina">
+                <img src="<?php echo $bttf->poster; ?>" alt="<?php echo 'Locandina di' . ' ' . $bttf->title ?>">
+            </div>
+            <div class="movie_data">
+                <h2>Titolo: <?php echo $bttf->title; ?></h2>
+                <p>Genere: <?php echo $bttf->genre; ?></p>
+                <p>Regista: <?php echo $bttf->director; ?></p>
+                <p class="horrible_truth">Uscito al cinema <span><?php echo $bttf->howLongAgo(); ?></span> anni fa</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="locandina">
+                <img src="<?php echo $labirintoDelFauno->poster; ?>" alt="<?php echo 'Locandina di' . ' ' . $labirintoDelFauno->title ?>">
+            </div>
+            <div class="movie_data">
+                <h2>Titolo: <?php echo $labirintoDelFauno->title; ?></h2>
+                <p>Genere: <?php echo $labirintoDelFauno->genre; ?></p>
+                <p>Regista: <?php echo $labirintoDelFauno->director; ?></p>
+                <p class="horrible_truth">Uscito al cinema <span><?php echo $labirintoDelFauno->howLongAgo(); ?></span> anni fa</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="locandina">
+                <img src="<?php echo $tron->poster; ?>" alt="<?php echo 'Locandina di' . ' ' . $tron->title ?>">
+            </div>
+            <div class="movie_data">
+                <h2>Titolo: <?php echo $tron->title; ?></h2>
+                <p>Genere: <?php echo $tron->genre; ?></p>
+                <p>Regista: <?php echo $tron->director; ?></p>
+                <p class="horrible_truth">Uscito al cinema <span><?php echo $tron->howLongAgo(); ?></span> anni fa</p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
